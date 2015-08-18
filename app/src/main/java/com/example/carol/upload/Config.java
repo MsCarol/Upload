fd0fbe4c3c220f0ff2800bbc9bc23f9ef6a9e794
MsCarol <kcarolyne7@gmail.com>
2015-08-18 11:43:11 +0300

    Upload
    Takes pictures from Camera. Need to do so from gallery.

---
+++ app/src/main/java/com/example/carol/upload/Config.java
@@ -0,0 +1,9 @@
+package com.example.carol.upload;
+
+public class Config {
+	// File upload url (replace the ip with your server address)
+	public static final String FILE_UPLOAD_URL = "http://yzarint.com/ok/fileUpload.php";
+	
+	// Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+}
